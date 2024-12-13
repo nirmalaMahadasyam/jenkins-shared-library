1816ab59e34fd556a6fe7ae24e84f86a962588ed
nirmalaMahadasyam <nirmalaMahadasyam@github.com>
2024-12-13 21:53:48 +0530

    shared library pipeline

--- vars/pipelineGlobals.groovy
+++ vars/pipelineGlobals.groovy
@@ -1,3 +1,4 @@
+// this code is for 78s
 static final String region(){
     return "us-east-1"
 }
@@ -6,4 +7,22 @@ static final String nexusURL(){
 }
 static final String account_id(){
     return "315069654700"
+}
+// this code is for 81s
+
+def getAccountID(String environment){
+    switch(environment) { 
+        case 'dev': 
+            return "315069654700"
+        case 'qa':
+            return "315069654700"
+        case 'uat':
+            return "315069654700"
+        case 'pre-prod':
+            return "315069654700"
+        case 'prod':
+            return "315069654700"
+        default:
+            return "nothing"
+    } 
 }
